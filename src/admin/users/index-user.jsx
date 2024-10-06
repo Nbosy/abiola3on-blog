e1ddfa0a3d9da87dac1dd072788b7ad6608754b4
matmaxgraphics <gbadamosimateen@gmail.com>
2024-10-06 08:50:25 +0100

    configured admin level permission to list users

--- src/admin/users/index-user.jsx
+++ src/admin/users/index-user.jsx
@@ -1,25 +1,44 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import PanelMainLayout from "../../layout/PanelMainLayout";
 import AdminLoader from "../../components/AdminLoader";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+const API_URL = "https://abiola3on-blog-backend.onrender.com";
+
 const ManageUser = function () {
   const location = useLocation();
   const navigate = useNavigate();
 
+  const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (location.state?.message) {
       toast.success(location.state.message);
 
       navigate(location.pathname, { replace: true });
     }
-  }, [location.state])
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(`${API_URL}/users`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await response.json();
+        console.log("Users:", data);
+        setUsers(data.users.users);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
 
-
+    fetchUsers();
+  }, [location.state]);
+  console.log(users);
 
   return (
     <PanelMainLayout>
@@ -44,20 +63,26 @@ const ManageUser = function () {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td data-label="S/N">1</td>
-              <td data-label="Username">Matmax</td>
-              <td data-label="Role">Admin</td>
-              <td data-label="Status">Verified</td>
-              <td data-label="Action">
-                <a href="#" className="delete">delete</a>
-              </td>
-            </tr>
+            {users.map((user, index) => (
+              <tr key={user.id}>
+                <td data-label="S/N">{index + 1}</td>
+                <td data-label="Email">{user.email}</td>
+                <td data-label="Role">{user.user_metadata?.role || "User"}</td>
+                <td data-label="Status">
+                  {user.confirmed_at ? "Active" : "Inactive"}
+                </td>
+                <td data-label="Action">
+                  <a href="#" className="delete">
+                    Delete
+                  </a>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
 
-      <ToastContainer/>
+      <ToastContainer />
     </PanelMainLayout>
   );
 };
